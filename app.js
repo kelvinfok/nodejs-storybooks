821ca8964540dd2d897c1b4fa845570421058f89
kelvinfok <punkd1@gmail.com>
2018-12-26 12:41:22 +0800

    handle google auth

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+const auth = require('./routes/auth');
+
+// Passport Config
+require('./config/passport')(passport);
+
+
+
 const port = process.env.PORT || 3000;
 
 const app = express();
 
+// Use Routes
+app.use('/auth', auth);
+
 app.get('/', (req, res) => {
     res.send('Works');
 })
+
+
 
 app.listen(port, ()=> {
     console.log(`Server started on port ${port}`);
---
+++ config/keys.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+    mongoURI: 'mongodb://kelvinfok:Password123@ds143614.mlab.com:43614/storybooks-dev',
+    googleClientID: '682222869496-86kubd3ltqj9hq1rnpgi1av0gu8herii.apps.googleusercontent.com',
+    googleClientSecret: 'y8hmlE2BHT3B9J75qhm3-JKX'
+}
